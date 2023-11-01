d545b6e3d16dfabe61118250bef2a76eb76efa53
James Robinson <jrstats@outlook.com>
2023-11-01 23:21:07 +0000

    add plot many

--- helpers/plot.py
+++ helpers/plot.py
@@ -4,7 +4,27 @@ def plot_team(elo_df, team):
     (
         elo_df
         .loc[lambda df: df["team"] == team]
-        .set_index("datetime")
+        .set_index(["season", "game_number"])
+        ["elo_team_post"]
+        .plot()
+    )
+
+def plot_many(elo_df, teams):
+    # TODO: doesn't seem to put indexes together properly if tenure is misaligned
+    (
+        elo_df
+        .loc[lambda df: df["team"].isin(teams)]
+        .set_index(["season", "game_number"])
+        .groupby("team")
+        ["elo_team_post"]
+        .plot()
+    )
+
+def plot_all(elo_df):
+    (
+        elo_df
+        .set_index(["season", "game_number"])
+        .groupby("team")
         ["elo_team_post"]
         .plot()
     )
